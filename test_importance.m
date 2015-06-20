38fdbdd275ecc68e93fbfecf587f2f10b8931a63
zhangchi8620 <czhang24@utk.edu>
2015-06-20 14:58:41 -0400

    rm all test m files

--- test_importance.m
+++
@@ -1,9 +0,0 @@
-function result = test_importance(vector, value)
-%     x = vector .* vector .* repmat(value',5, 1);
-%     result = sqrt(sum(x, 2));
-    x = vector .* repmat(value',size(vector, 1), 1);
-    result = sqrt(sum(x.*x, 2));
-%     result
-%     figure;
-    bar(result);
-end
--- test_struct.m
+++
@@ -1,5 +0,0 @@
-field1 = 'action';  value1 = 0;
-field2 = 'instance';  value2 = 0;
-field3 = 'data';  value3 = [];
-
-s = struct(field1,value1,field2,value2,field3,value3);
